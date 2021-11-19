0761f7e53d8174f333ab6431e75342ae4fd882c6
Mustafa Basaran <52412603+mustafa-basaran@users.noreply.github.com>
2021-11-18 21:06:19 -0500

    CSV to bacteria with examples

---
+++ GRO_Scripts/csv_to_bacteria.m
@@ -0,0 +1,24 @@
+%%For exporting csv to matlab file, you should drag to csv file to matlab
+%%and import it as a numeric matrix, also you should select the option,
+%%replace nonimportable cells with NaN
+%%For example in here G4 is exported version of G4.5R4.5_datas.csv
+
+alldatas= G4;%%Here you should write name of the imported numeric matrix
+firstcolumn = alldatas(:,1);
+indexofnans = find(isnan(firstcolumn)==1);
+indexofnans = [indexofnans ; (length(firstcolumn)+1)];
+r = 0.5;
+bacteria_colonies_cell = cell(1,1);
+for i = 1 :(length(indexofnans)-1)
+    bacteria = zeros(indexofnans(i+1)-indexofnans(i)-1,6);
+    currentdatas = alldatas((indexofnans(i)+1):(indexofnans(i+1)-1),:);
+    bacteria(:,1) = currentdatas(:,2);
+    bacteria(:,2) = currentdatas(:,3);
+    bacteria(:,4) = currentdatas(:,4);
+    bacteria(:,3) = (currentdatas(:,5))/(pi*r*r);
+    bacteria(:,6) = (currentdatas(:,7)>currentdatas(:,6))+1;
+    bacteria(:,1) = bacteria(:,1)*1e-7;
+    bacteria(:,2) = bacteria(:,2)*1e-7;
+    bacteria(:,3) = (bacteria(:,3)-1)*1e-6;
+    bacteria_colonies_cell{i}=bacteria;
+end
